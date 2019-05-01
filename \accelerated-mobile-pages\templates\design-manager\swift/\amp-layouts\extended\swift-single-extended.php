a883c9d3a5833c5c9dc069b162ac0de0c0ca0c0b
Sarah Calande <40167185+sarahcalande@users.noreply.github.com>
2019-05-01 13:44:29 -0400

    Create \amp-layouts\extended\swift-single-extended.php

---
+++ \accelerated-mobile-pages\templates\design-manager\swift/\amp-layouts\extended\swift-single-extended.php
@@ -0,0 +1,4 @@
+<div class="sd-5 sgl">
+	            <a href="https://talentrecap.com/vote-who-should-be-the-next-american-idol-2019-take-our-poll-now/amp">
+<amp-img class="alignnone size-full wp-image-17436" src="https://talentrecap.com/wp-content/uploads/2019/04/ampbanner.png" width='2464px' height='445px' layout="responsive"></amp-img></a>
+		<div class="cntr">
